72fbd5edcc704dd4be3208d471ca13c23db47861
Andreas Kollegger <akollegger@Fatty-MacBook.local>
2009-09-24 13:16:54 -0400

    initial impl of db4o dao

--- org.gatherdata.forms.dao.pojo/src/main/java/org/gatherdata/forms/dao/pojo/osgi/OSGiActivator.java
+++ org.gatherdata.forms.dao.pojo/src/main/java/org/gatherdata/forms/dao/pojo/osgi/OSGiActivator.java
@@ -1,10 +1,14 @@
 package org.gatherdata.forms.dao.pojo.osgi;
 
-import java.util.Dictionary;
-import java.util.Properties;
+import static com.google.inject.Guice.createInjector;
+import static org.ops4j.peaberry.Peaberry.osgiModule;
 
+import org.gatherdata.forms.core.spi.FormTemplateDao;
+import org.ops4j.peaberry.Export;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+
+import com.google.inject.Inject;
 
 /**
  * Extension of the default OSGi bundle activator
@@ -12,12 +16,16 @@ import org.osgi.framework.BundleContext;
 public final class OSGiActivator
     implements BundleActivator
 {
+	@Inject
+	Export<FormTemplateDao> dao;
+	
     /**
      * Called whenever the OSGi framework starts our bundle
      */
     public void start( BundleContext bc )
         throws Exception
     {
+		createInjector(osgiModule(bc), new GuiceBindingModule()).injectMembers(this);
     }
 
     /**
--- org.gatherdata.forms.dao.pojo/src/test/java/org/gatherdata/forms/dao/pojo/internal/FormTemplateDaoPojoTest.java
+++ org.gatherdata.forms.dao.pojo/src/test/java/org/gatherdata/forms/dao/pojo/internal/FormTemplateDaoPojoTest.java
@@ -5,30 +5,12 @@ import static org.junit.Assert.assertTrue;
 import org.gatherdata.commons.net.CbidFactory;
 import org.gatherdata.commons.spi.BaseStorageDaoTest;
 import org.gatherdata.forms.core.model.FormTemplate;
-import org.gatherdata.forms.core.model.MutableFormTemplate;
+import org.gatherdata.forms.core.model.impl.MutableFormTemplate;
+import org.gatherdata.forms.core.spi.BaseFormTemplateDaoTest;
 import org.joda.time.DateTime;
 import org.junit.Test;
 
-public class FormTemplateDaoPojoTest extends BaseStorageDaoTest<FormTemplate, FormTemplateDaoPojo> {
-
-    private int mockCount = 0;
-
-    @Override
-    protected FormTemplate createMockEntity() {
-        MutableFormTemplate mockTemplate = new MutableFormTemplate();
-        mockTemplate.setUid(CbidFactory.createCbid("mockFormTemplate #:" + mockCount));
-        DateTime dateModified = new DateTime();
-        DateTime dateCreated = dateModified.minusMinutes(1);
-        mockTemplate.setDateCreated(dateCreated);
-        mockTemplate.setDateModified(dateModified);
-        mockTemplate.setDescription("a mock form template #" + mockCount);
-        mockTemplate.setFormStyle("mock");
-        mockTemplate.setFormTemplate("name: %x");
-        mockTemplate.setFormType("text/plain");
-        mockTemplate.setName("mock #" + mockCount);
-        mockCount++;
-        return mockTemplate;
-    }
+public class FormTemplateDaoPojoTest extends BaseFormTemplateDaoTest {
 
     @Override
     protected FormTemplateDaoPojo createStorageDaoImpl() {
